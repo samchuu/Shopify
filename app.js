f8c39de9214e6d6c61fa0e82a90bd0339f0e071b
Sam Chu <samchupiano@gmail.com>
2021-08-25 21:14:08 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,89 @@
+const hamburger = document.querySelector(".hamburger");
+const sideMenu = document.querySelector(".side__menu");
+const shopCategories = document.querySelector(".show__menu");
+const innerMenu = document.querySelector(".nav__inner__menu");
+const desktopInnerMenu = document.querySelector(
+  ".desktop__navigation__lower ul"
+);
+const mobileShift = document.querySelector(".shift__wrapper");
+const stickyNavigation = document.querySelector(".sticky__navigation__wrapper");
+const smallImages = document.querySelectorAll("div.koiski__image");
+const categoryWrapper = document.querySelector(".categories__wrapper");
+const koiskiWrapper = document.querySelector(".koiski__wrapper");
+// sticky navigation
+window.addEventListener("scroll", () => {
+  stickyNavigation.classList.toggle("sticky__open", window.scrollY > 200);
+});
+
+window.addEventListener("scroll", (e) => {
+  console.log(window.scrollY, window.innerWidth);
+  if (window.innerWidth < 875) {
+    categoryWrapper.classList.toggle("categories__show", window.scrollY > 1242);
+    koiskiWrapper.classList.toggle(
+      "koiski__wrapper__show",
+      window.scrollY > 5750
+    );
+  } else if (window.innerWidth < 1366) {
+    categoryWrapper.classList.toggle("categories__show", window.scrollY > 942);
+    koiskiWrapper.classList.toggle(
+      "koiski__wrapper__show",
+      window.scrollY > 2707
+    );
+  } else if (window.innerWidth > 1367) {
+    categoryWrapper.classList.toggle("categories__show", window.scrollY > 1260);
+    koiskiWrapper.classList.toggle(
+      "koiski__wrapper__show",
+      window.scrollY > 3081
+    );
+  }
+});
+// show hamburger menu
+function toggleNav() {
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("rotate");
+    sideMenu.classList.toggle("open");
+    mobileShift.classList.toggle("shift");
+    innerMenu.classList.remove("inner__mobile__open");
+    // remove scroll when modal is active
+    document.querySelector("body").classList.toggle("active");
+  });
+}
+
+function showInnerMenu() {
+  shopCategories.addEventListener("click", () => {
+    innerMenu.classList.toggle("inner__mobile__open");
+  });
+}
+
+function showDesktopInnerMenu() {
+  desktopInnerMenu.addEventListener("click", () => {
+    innerMenu.classList.toggle("inner__desktop__open");
+  });
+}
+
+// koiski images
+for (let small of smallImages) {
+  small.addEventListener(
+    "mouseover",
+    () => {
+      const answer = small.firstElementChild;
+      answer.style.display = "block";
+    },
+    false
+  );
+}
+
+for (let small of smallImages) {
+  small.addEventListener(
+    "mouseout",
+    () => {
+      const answer = small.firstElementChild;
+      answer.style.display = "none";
+    },
+    false
+  );
+}
+
+toggleNav();
+showInnerMenu();
+showDesktopInnerMenu();
