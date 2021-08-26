7112377ea4cebd6981c59e06fa68454005aa48ae
Sam Chu <samchupiano@gmail.com>
2021-08-26 10:42:39 +0200

    refactor

--- app.js
+++ app.js
@@ -3,38 +3,33 @@ const closeIcon = document.querySelector(".inner__close__icon");
 const sideMenu = document.querySelector(".side__menu");
 const shopCategories = document.querySelector(".show__menu");
 const innerMenu = document.querySelector(".nav__inner__menu");
-const desktopInnerMenu = document.querySelector(
-  ".desktop__navigation__lower ul"
-);
+const desktopInnerMenu = document.querySelector(".desktop__navigation__lower ul");
 const mobileShift = document.querySelector(".shift__wrapper");
 const stickyNavigation = document.querySelector(".sticky__navigation__wrapper");
 const smallImages = document.querySelectorAll("div.koiski__image");
 const categoryWrapper = document.querySelector(".categories__wrapper");
 const koiskiWrapper = document.querySelector(".koiski__wrapper");
+
 // sticky navigation
 window.addEventListener("scroll", () => {
   stickyNavigation.classList.toggle("sticky__open", window.scrollY > 200);
 });
 
-window.addEventListener("scroll", (e) => {
-  console.log(window.scrollY, window.innerWidth);
+// when browser scroll height reaches a certain point, animation takes place
+function animation() {
   if (window.innerWidth < 875) {
     categoryWrapper.classList.toggle("categories__show", window.scrollY > 1242);
-    koiskiWrapper.classList.toggle(
-      "koiski__wrapper__show",
-      window.scrollY > 5750
-    );
+    koiskiWrapper.classList.toggle("koiski__wrapper__show",window.scrollY > 5750);
   } else if (window.innerWidth < 1366) {
     categoryWrapper.classList.toggle("categories__show", window.scrollY > 942);
     koiskiWrapper.style.display = "flex";
   } else if (window.innerWidth > 1367) {
     categoryWrapper.classList.toggle("categories__show", window.scrollY > 1260);
-    koiskiWrapper.classList.toggle(
-      "koiski__wrapper__show",
-      window.scrollY > 3081
-    );
+    koiskiWrapper.classList.toggle("koiski__wrapper__show", window.scrollY > 3081);
   }
-});
+}
+window.addEventListener("scroll", animation);
+
 // show hamburger menu
 function toggleNav() {
   hamburger.addEventListener("click", () => {
@@ -50,13 +45,13 @@ function toggleNav() {
     innerMenu.classList.remove("inner__mobile__open");
   });
 }
-
+// mobile
 function showInnerMenu() {
   shopCategories.addEventListener("click", () => {
     innerMenu.classList.toggle("inner__mobile__open");
   });
 }
-
+//desktop 
 function showDesktopInnerMenu() {
   desktopInnerMenu.addEventListener("click", () => {
     innerMenu.classList.toggle("inner__desktop__open");
